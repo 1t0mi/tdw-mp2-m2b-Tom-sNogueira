bb0b28ab45985b861c6a536b58538f092e73c2ce
Tomás <129265596+1t0mi@users.noreply.github.com>
2025-12-02 17:58:16 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: "/tdw-mp2-m2b-Tom-sNogueira/", // O nome exato do teu repositório entre barras
+})
